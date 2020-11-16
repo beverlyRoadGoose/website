408386177210708128c3849f49d145dcff83acc7
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2020-11-16 16:15:33 +0100

    move hello to index

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,8 @@
 import Vue from 'vue';
 import Blog from '@/views/Blog';
-import Me from '@/assets/me.jpg';
 import VueRouter from 'vue-router';
 import Home from '@/views/Home.vue';
-import Hello from '@/views/Hello.vue';
 import BlogPost from '@/views/BlogPost';
-import { preloadImage } from '@/util/preloadImage';
 import { createDefaultThemeCookie } from '@/util/createDefaultThemeCookie';
 
 Vue.use(VueRouter);
@@ -17,17 +14,6 @@ const routes = [
     component: Home,
     // eslint-disable-next-line no-unused-vars
     beforeEnter(to, from, next) {
-      createDefaultThemeCookie();
-      next();
-    }
-  },
-
-  {
-    path: '/hello',
-    name: 'hello',
-    component: Hello,
-    beforeEnter(to, from, next) {
-      preloadImage(Me);
       createDefaultThemeCookie();
       next();
     }
@@ -48,7 +34,6 @@ const routes = [
     name: 'blog-post',
     component: BlogPost,
     beforeEnter(to, from, next) {
-      preloadImage(Me);
       createDefaultThemeCookie();
       next();
     }
