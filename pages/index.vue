8e5de6429ed95445b104049875efa9426e9e3c70
Oluwatobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2021-03-10 15:14:54 +0100

    slight modifications to index view

--- pages/index.vue
+++ pages/index.vue
@@ -21,12 +21,7 @@
           target="_blank"
           @click="recordNtlClickEvent"
         >
-          <b>The National</b></a
-        >
-        every chance I get. Occasionally,
-        <NuxtLink to="/posts"><b> I write</b></NuxtLink
-        >. <br /><br />
-        If you'd like to get in touch, feel free to
+          <b>The National</b></a> every chance I get. If you'd like to get in touch, feel free to
         <a href="mailto:hey@heytobi.dev"><b>write me</b></a>.
       </span>
 
@@ -47,26 +42,6 @@
             <title>LinkedIn</title>
             <path
               d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"
-            />
-          </svg>
-        </a>
-
-        <a
-          href="https://stackoverflow.com/users/story/4961714"
-          title="StackOverflow"
-          class="find-me"
-          :style="findMeStyle"
-          target="_blank"
-          @click="recordStackOverflowEvent"
-        >
-          <svg
-            role="img"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Stack Overflow</title>
-            <path
-              d="M18.986 21.865v-6.404h2.134V24H1.844v-8.539h2.13v6.404h15.012zM6.111 19.731H16.85v-2.137H6.111v2.137zm.259-4.852l10.48 2.189.451-2.07-10.478-2.187-.453 2.068zm1.359-5.056l9.705 4.53.903-1.95-9.706-4.53-.902 1.936v.014zm2.715-4.785l8.217 6.855 1.359-1.62-8.216-6.853-1.35 1.617-.01.001zM15.751 0l-1.746 1.294 6.405 8.604 1.746-1.294L15.749 0h.002z"
             />
           </svg>
         </a>
