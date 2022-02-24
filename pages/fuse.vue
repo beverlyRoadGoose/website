e108e568e696da3eac7a66cb2d6441e089d05af6
Oluwatobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2022-02-24 21:26:08 +0100

    add fuse lib redirect

---
+++ pages/fuse.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- This component only exists to allow the resolution of my fuse golang library to be resolved by the "go get" command -->
+</template>
+
+<script>
+export default {
+  name: 'fuse',
+  meta: [
+    { name: 'go-import', content: "golang.org/x/tools git http://github.com/beverlyRoadGoose/fuse" }
+  ]
+}
+</script>
