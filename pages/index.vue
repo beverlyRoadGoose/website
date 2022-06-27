f1a971895d71f933cf17464505f1d12291b42c99
Oluwatobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2022-06-27 16:21:47 +0200

    update current job

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
         />
       </transition>
       <span id="about" :style="aboutStyle">
-        I'm currently based out of Berlin, building stuff at <a href="https://www.hellofreshgroup.com/en/" target="_blank" @click="recordHelloFreshClickEvent" :style="aboutStyle"><b>HelloFresh</b></a>.
+        I'm currently based out of Berlin, building stuff at <a href="https://www.lifeatspotify.com/" target="_blank" @click="recordEmployerClickEvent" :style="aboutStyle"><b>Spotify</b></a>.
         Outside of programming, I enjoy discovering <a href="https://www.youtube.com/watch?v=TeaDE1magRk" target="_blank" @click="recordMusicClickEvent" :style="aboutStyle"><b>good music</b></a>.
         I share my favorites as curated playlists I like to call <NuxtLink to="/paracosms" :style="aboutStyle"><b>Paracosms</b></NuxtLink>.<sup><a href="https://en.wikipedia.org/wiki/Paracosm" target="_blank" :style="aboutStyle" style="text-decoration:none">?</a></sup>
       </span>
@@ -186,10 +186,10 @@ export default {
       });
     },
 
-    recordHelloFreshClickEvent() {
-      this.$gtag.event('clickedHelloFresh', {
+    recordEmployerClickEvent() {
+      this.$gtag.event('clickedEmployer', {
         event_category: 'externalLinkClick',
-        event_label: 'helloFreshClick',
+        event_label: 'employerClick',
         value: 1
       });
     },
